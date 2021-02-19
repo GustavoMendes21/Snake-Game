304c4244271f87661de10eacb95463b59314accd
Gustavo Mendes <gustavomendes2112@hotmail.com>
2021-02-19 14:45:47 -0300

    snake-created

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,9 +2,24 @@ const canvas = document.getElementById("snake");
 const context = canvas.getContext("2d");
 const box = 32;
 
+const snake = []
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
+
 function createBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0,0,16*box,16*box)
 }
 
+function createSnake() {
+    for(i=0; i < snake.length; i++) {
+        context.fillStyle = "#363b96"
+        context.fillRect(snake[i].x, snake[i].y, box, box)
+    }
+}
+
 createBG();
+createSnake();
