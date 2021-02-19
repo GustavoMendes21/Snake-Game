2d21a0760bef5cdfe6cdbfd7fd288dca61c83330
Gustavo Mendes <gustavomendes2112@hotmail.com>
2021-02-19 14:58:26 -0300

    snake-moviment

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,7 +7,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
-
+let direction = "right"
 
 function createBG() {
     context.fillStyle = "lightgreen";
@@ -21,5 +21,26 @@ function createSnake() {
     }
 }
 
-createBG();
-createSnake();
+function initGame() {
+    createBG();
+    createSnake();
+    
+    let snakeX = snake[0].x
+    let snakeY = snake[0].y
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY += box;
+    if(direction == "down") snakeY -= box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY,
+    }
+
+    snake.unshift(newHead)
+}
+
+//const gameInterval = setInterval(initGame, 100);
