6f15deb3e62c467a80c2af3eacaa5bc6a1389fee
Gustavo Mendes <gustavomendes2112@hotmail.com>
2021-02-19 16:15:09 -0300

    game-full-complete

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -21,7 +21,7 @@ function createBG() {
 
 function createSnake() {
     for(i=0; i < snake.length; i++) {
-        context.fillStyle = "#363b96"
+        context.fillStyle = "#3c009c"
         context.fillRect(snake[i].x, snake[i].y, box, box)
     }
 }
@@ -41,11 +41,20 @@ document.addEventListener("keydown", btn => {
 });
 
 function initGame() {
+    
+    
     if (snake[0].x > 15*box && direction == "right") snake[0].x = 0;
     if (snake[0].x < 0 && direction == "left") snake[0].x = 16*box;
     if (snake[0].y > 15*box && direction == "down") snake[0].y = 0;
     if (snake[0].y < 0 && direction == "up") snake[0].y = 16*box;
     
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(gameInterval)
+            alert("Game Over, para reinciar recarregue a página")
+        }
+    }
+
     createBG();
     createSnake();
     drawFood()
@@ -58,7 +67,13 @@ function initGame() {
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }else{
+        food.x =  Math.floor(Math.random() * 15 + 1) *box;
+        food.y =  Math.floor(Math.random() * 15 + 1) *box;
+        
+    }
 
     let newHead = {
         x: snakeX,
